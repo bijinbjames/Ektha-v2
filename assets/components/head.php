7c761470dc7d2b2da56dd445ae2e87dafd1eafcf
bijinbjames <bijinbjames@gmail.com>
2023-04-18 18:55:26 +0530

    scale

--- assets/components/head.php
+++ assets/components/head.php
@@ -1,7 +1,7 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=0.8">
+  <meta meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
   <title>Ektha23</title>
   <link rel="shortcut icon" href="./favicon.svg" type="image/svg+xml">
   <link rel="stylesheet" href="./assets/css/style.css">
